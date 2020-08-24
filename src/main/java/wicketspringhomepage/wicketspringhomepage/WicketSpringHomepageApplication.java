99a920ea2913a127ad7b5eea14f9160f7872b46a
YoshikiYamada <yoshikiyamada@usernoMacBook-puro.local>
2020-08-24 23:16:49 +0900

    create heroku

---
+++ src/main/java/wicketspringhomepage/wicketspringhomepage/WicketSpringHomepageApplication.java
@@ -0,0 +1,13 @@
+package wicketspringhomepage.wicketspringhomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WicketSpringHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WicketSpringHomepageApplication.class, args);
+	}
+
+}
---
+++ src/main/java/wicketspringhomepage/wicketspringhomepage/page/HomePage.java
@@ -0,0 +1,10 @@
+package wicketspringhomepage.wicketspringhomepage.page;
+
+import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
+import org.apache.wicket.markup.html.WebPage;
+import org.wicketstuff.annotation.mount.MountPath;
+
+@WicketHomePage
+@MountPath("Home")
+public class HomePage extends WebPage {
+}
